69a8c6b3df3c329850d1602b7d78a9def7da47b4
Mikhail Volodin <volodinmiisha1@gmail.com>
2024-06-12 12:00:49 +0300

    Added button, when you can choose what you want to add (file or url) for Description your thing.

--- src/app/components/MainContent.tsx
+++ src/app/components/MainContent.tsx
@@ -11,7 +11,7 @@ const MainContent = () => {
   const router = useRouter()
 
   const [allThings, setAllThings] = useState<Thing[]>([])
-  
+
 
   useEffect(() => {
     const fetchThings = async () => {
@@ -31,25 +31,23 @@ const MainContent = () => {
   }
 
   return (
-    <section>
-      <section className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center align-items-center'>
-        {
-          allThings.map(thing => (
-            <div className='rounded-lg releative h-72 flex flex-col cursor-pointer' key={thing.id} onClick={() => handleClick(thing.id)}>
-              <p className='relative h-72'>
-                {
-                  thing.photothing ? (
-                    <Image src={thing.photothing as string} alt={thing.name as string} width={250} height={250} className='rounded-lg' />
-                  ) : null
-                }
-              </p>
-              <p className='text-xl font-bold'>{thing.name}</p>
-              <p className='text-lg'>{thing.country}, {thing.city}</p>
-            </div>
-          ))
-        }
-      </section>
-    </section>
+    <div className='w-full h-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 justify-items-center align-items-center'>
+      {
+        allThings.map(thing => (
+          <section className=' rounded-lg releative h-80 w-72 flex flex-col cursor-pointer' key={thing.id} onClick={() => handleClick(thing.id)}>
+            <p className='relative h-72'>
+              {
+                thing.photothing ? (
+                  <Image src={thing.photothing as string} alt={thing.name as string} width={250} height={250} className='rounded-lg h-full w-full object-cover' />
+                ) : null
+              }
+            </p>
+            <p className='text-xl font-bold'>{thing.name}</p>
+            <p className='text-lg'>{thing.country}, {thing.city}</p>
+          </section>
+        ))
+      }
+    </div>
   )
 }
 
